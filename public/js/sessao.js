c1f08faf0607aff31b889124b4c15938b4aad1cd
isabelafrancisco <isabelafrancisco@sptech.school>
2024-11-26 09:58:23 -0300

    projeto finalizado

--- public/js/sessao.js
+++ public/js/sessao.js
@@ -25,11 +25,11 @@ function aguardar() {
 
 function finalizarAguardar(texto) {
     var divAguardar = document.getElementById("div_aguardar");
-    divAguardar.style.display = "none";
+    // divAguardar.style.display = "none";
 
     var divErrosLogin = document.getElementById("div_erros_login");
     if (texto) {
-        divErrosLogin.style.display = "flex";
+        // divErrosLogin.style.display = "flex";
         divErrosLogin.innerHTML = texto;
     }
 }
